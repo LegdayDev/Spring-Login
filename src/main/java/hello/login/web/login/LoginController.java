a5546a7d70e75bf1ce4fbf893b09834804aa131e
Legday <chlwodud0327@naver.com>
2023-10-14 16:09:03 +0900

    Feat: 로그인/로그아웃 구현
    로그인 구현 - 회원 Id값으로 Cookie 생성후 Response 헤더에 담아서 클라이언트에게 응답
    로그아웃 구현 - 현재 저장된 쿠키이름과 똑같은 이름으로 쿠키를 생성하고 생성시간을 0으로 만들어서 Response 헤더에 담아서 응답

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,18 +27,23 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
+        // Field Error
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
+
         Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
 
+        // Global Error
         if(loginMember == null){
             bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리(세션 쿠키) -> 브라우저 종료 시 모두 종료
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
     }
