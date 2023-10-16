488d767c7c6b2dcb3e851da7804782fa26f69cdc
Legday <chlwodud0327@naver.com>
2023-10-16 17:50:52 +0900

    Feat: @SessionAttribute
    스프링에서 제공한느 @SessionAttribute 를 사용하여 번거로운 작업이 사라졌다.

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -58,7 +59,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);
@@ -70,6 +71,18 @@ public class HomeController {
         if(loginMember == null)
             return "home";
 
+
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name =SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+
+        // Session 에 회원 데이터가 없으면 home
+        if(loginMember == null)
+            return "home";
 
         model.addAttribute("member",loginMember);
         return "loginHome";
